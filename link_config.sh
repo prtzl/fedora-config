2086e1474681b9991a212a76afcaba14f2ccba4d
prtzl <matej.blagsic@protonmail.com>
2022-02-22 08:08:43 -0500

    Fix link creation

--- link_config.sh
+++ link_config.sh
@@ -3,19 +3,20 @@
 pushd dotfiles
 
 # Alacritty
-rm -rf ~/.config/Alacritty/*
-ln -s alacritty.yml ~/.config/Alacritty/alacritty.yml
+rm -rf ~/.config/Alacritty
+mkdir -p ~/.config/Alacritty
+ln -s $PWD/alacritty.yml ~/.config/Alacritty/alacritty.yml
 
 # Starship
 rm -f ~/.config/starship.toml
-ln -s starship.toml ~/.config/starship.toml
+ln -s $PWD/starship.toml ~/.config/starship.toml
 
 # Zsh
 rm -f ~/.zshrc
-ln -s .zshrc ~/.zshrc
+ln -s $PWD/.zshrc ~/.zshrc
 
 # Git
 rm -f ~/.gitconfig
-ln -s .gitconfig ~/.gitconfig
+ln -s $PWD/.gitconfig ~/.gitconfig
 
 popd
