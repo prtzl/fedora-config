d7817fd9fbbb1914d08e7b5df5eff48335eacb4f
prtzl <matej.blagsic@protonmail.com>
2022-06-20 12:06:29 +0200

    Group installs, fix bugs, add options to script

--- init_install.sh
+++ init_install.sh
@@ -1,37 +1,55 @@
 #! /usr/bin/env sh
 
-packages_shell= "zsh tmux zsh-syntax-highlighting starship alacritty fzf \
+packages_shell="zsh tmux zsh-syntax-highlighting starship alacritty fzf \
                 zsh-autosuggestions direnv neovim"
 
-packages_media= "eog celluloid vlc xreader transmission gimp chromium \
+packages_media="eog celluloid vlc xreader transmission gimp chromium \
                 skypeforlinux texlive-scheme-full texstudio"
 
-packages_tools= "git net-tools xclip util-linux-user dnf-plugins-core flatpak \
+packages_tools="git net-tools xclip util-linux-user dnf-plugins-core flatpak \
                 megasync tio neofetch htop subversion p7zip redshift \
                 wmctrl xdotool"
 
-packages_virt=  "bridge-utils virt-manager libvirt virt-install qemu-kvm \
-                libvirt-devel virt-top libguestfs-tools guestfs-tools"
+packages_virt="bridge-utils virt-manager libvirt virt-install qemu-kvm \
+                libvirt-devel virt-top libguestfs-tools guestfs-tools \
+                docker-ce docker-ce-cli containerd.io"
 
-packages_lib=   "ncurses-compat-libs"
+packages_lib="ncurses-compat-libs"
 
-packages_dev=   "arm-none-eabi-gcc-cs arm-none-eabi-gcc-cs-c++ arm-none-eabi-newlib \
+packages_dev="arm-none-eabi-gcc-cs arm-none-eabi-gcc-cs-c++ arm-none-eabi-newlib \
                 gcc g++ make cmake clang-tools-extra\
                 python3 python3-pip \
                 stlink valgrind"
 
+command=$1
+if [[ -z "$command" || ("$command" != "packages" && "$command" != "all") ]]; then
+    echo "Command: ./init_install.sh <all/packages>"
+    exit 1
+fi
+
 # Install basic dev packages
-sudo dnf check-update
-sudo dnf update -y
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y $packages_shell $packages_media $packages_tools \
-                    $packages_virt $packages_lib $packags_dev
+dnf check-update
+echo Installing packages!
+sudo sh -c "
+dnf update -y
+dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y $packages_shell $packages_media $packages_tools $packages_virt $packages_lib $packags_dev
+"
+
+if [[ "$command" == "packages" ]]; then
+    echo "Installed all packages!"
+    exit
+fi
 
 # Users and groups
+echo "Setting groups for $USER"
 groups="dialout audio video networkmanager libvirtd docker"
 for group in $groups; do
-    sudo groupadd -r $group;
+sudo sh -c "
+    sudo groupadd -r $group
     sudo usermod -aG $group $USER
+"
 done
 
 # Install flatpak applications
@@ -40,10 +58,10 @@ flatpak --user -yyy install com.mattermost.Desktop
 flatpak --user -yyy install flathub us.zoom.Zoom
 
 # Install docker
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install -yy docker-ce docker-ce-cli containerd.io
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo -sh -c "
+systemctl enable docker
+systemctl start docker
+"
 
 # Install nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
